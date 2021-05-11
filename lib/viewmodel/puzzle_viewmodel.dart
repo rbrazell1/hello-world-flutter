faade227f82b9c6d60d7cbffef03bcafeefa9cfd
Russell Brazell <brazellrussell@gmail.com>
2021-05-11 16:01:56 -0600

    main part of view model finished

--- lib/viewmodel/puzzle_viewmodel.dart
+++ lib/viewmodel/puzzle_viewmodel.dart
@@ -7,6 +7,7 @@ import 'package:hello_world_flutter/model/tile.dart';
 class PuzzleViewModel {
   static const _size = 28;
   static const _tick = 500;
+  static const _reveal = 3000;
 
   final _tileListStreamController;
   final _moveCountStreamController;
@@ -15,7 +16,8 @@ class PuzzleViewModel {
   final _rng;
 
   late Puzzle _puzzle;
-  Timer? _timer;
+  Timer? _tickTimer;
+  Timer? _revealTimer;
 
   PuzzleViewModel()
       : _tileListStreamController = StreamController<List<Tile>>(),
@@ -35,12 +37,29 @@ class PuzzleViewModel {
 
   newPuzzle(int imagePoolSize) {
     _puzzle = Puzzle(_size, imagePoolSize, _rng);
-    _timer?.cancel();
-    _timer = Timer.periodic(Duration(microseconds: _tick),
+    _tickTimer?.cancel();
+    _tickTimer = Timer.periodic(Duration(microseconds: _tick),
         (timer) => _tickStreamController.add(timer.tick));
   }
 
   select(int tileIndex) {
+    PuzzleState previous = _puzzle.state;
+    _puzzle.select(tileIndex);
+    if (_puzzle.state != previous) {
+      _tileListStreamController.add(_puzzle.tiles);
+      _moveCountStreamController.add(_puzzle.moves);
+      _matchCountStreamController.add(_puzzle.matches);
+      if (_puzzle.state == PuzzleState.revealing) {
+        _revealTimer?.cancel();
+        _revealTimer =
+            Timer(Duration(microseconds: _reveal), () => _unreveal());
+      }
+    }
+  }
 
+  _unreveal() {
+    _puzzle.unreveal();
+    _tileListStreamController.add(_puzzle.tiles);
+    _moveCountStreamController.add(_puzzle.moves);
   }
 }
