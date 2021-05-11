35948cca15a3e8ce1b0c905cd66d57e642ab77c0
Russell Brazell <brazellrussell@gmail.com>
2021-05-11 16:30:35 -0600

    corrected back to hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_flutter/view/tile_images.dart';
-import 'package:hello_world_flutter/viewmodel/puzzle_viewmodel.dart';
 
-void main() => runApp(TileMatchApp());
+void main() => runApp(MyApp());
 
-class TileMatchApp extends StatelessWidget {
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tile Match',
+      title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.red,
         accentColor: Colors.indigoAccent,
       ),
-      home: TileMatchScreen(title: 'Tile Match'),
+      home: MyHomePage(title: 'Home Page'),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class TileMatchScreen extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
   final String title;
 
-  TileMatchScreen({Key? key, required this.title}) : super(key: key);
-
   @override
-  _TileMatchScreenState createState() => _TileMatchScreenState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _TileMatchScreenState extends State<TileMatchScreen> {
-  final _viewmodel = PuzzleViewModel();
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  _TileMatchScreenState() {
-    _viewmodel.newPuzzle(tileImages.length);
+  void _incrementCounter() {
+    setState(() => _counter++);
   }
 
   @override
@@ -45,13 +63,18 @@ class _TileMatchScreenState extends State<TileMatchScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            StreamBuilder<int>(
-              stream: _viewmodel.tickStream,
-              builder: (context, snapshot) => Text('${snapshot.data}'),
+            Text(
+              'Why have you pushed the button $_counter times!?',
+              style: Theme.of(context).textTheme.subtitle1,
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
