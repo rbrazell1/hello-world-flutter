f526cf7baec5a4305aec4022f896c0cf3fabec0d
Russell Brazell <brazellrussell@gmail.com>
2021-05-11 15:48:48 -0600

    added icons list

--- lib/viewmodel/puzzle_viewmodel.dart
+++ lib/viewmodel/puzzle_viewmodel.dart
@@ -1,18 +1,28 @@
 import 'dart:async';
+import 'dart:math';
+
+import 'package:hello_world_flutter/model/puzzle.dart';
+import 'package:hello_world_flutter/model/tile.dart';
 
 class PuzzleViewModel {
+  static const _size = 28;
+  static const _tick = 500;
+
   final _tileListStreamController;
   final _moveCountStreamController;
   final _matchCountStreamController;
   final _tickStreamController;
+  final _rng;
+
+  late Puzzle _puzzle;
+  Timer? _timer;
 
   PuzzleViewModel()
       : _tileListStreamController = StreamController<List<Tile>>(),
         _moveCountStreamController = StreamController<int>(),
         _matchCountStreamController = StreamController<int>(),
-        _tickStreamController = StreamController<int>() {
-
-  }
+        _tickStreamController = StreamController<int>(),
+        _rng = Random.secure() {}
 
   Stream<List<Tile>> get tileListStream => _tileListStreamController.stream;
 
@@ -21,5 +31,16 @@ class PuzzleViewModel {
   Stream<int> get matchCountStreamController =>
       _matchCountStreamController.stream;
 
-  Stream<int> get tickStream => _tickStreamContoller.stream;
+  Stream<int> get tickStream => _tickStreamController.stream;
+
+  newPuzzle(int imagePoolSize) {
+    _puzzle = Puzzle(_size, imagePoolSize, _rng);
+    _timer?.cancel();
+    _timer = Timer.periodic(Duration(microseconds: _tick),
+        (timer) => _tickStreamController.add(timer.tick));
+  }
+
+  select(int tileIndex) {
+
+  }
 }
